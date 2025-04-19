1b346eacf2d505d7c3736e56d32a80b0cd99ec42
Charl Cronje <charlc@digitalcabinet.co.za>
2025-04-19 08:02:23 +0200

    Add admin functions for session tracking and analytics dashboard

--- admin/admin-functions.php
+++ admin/admin-functions.php
@@ -221,9 +221,9 @@ function formatDuration($seconds) {
     $seconds = $seconds % 60;
 
     if ($hours > 0) {
-        return sprintf('%d:%02d:%02d', $hours, $minutes, $seconds);
+        return sprintf('%d:%02d:%02d', (int)$hours, (int)$minutes, (int)$seconds);
     } else {
-        return sprintf('%d:%02d', $minutes, $seconds);
+        return sprintf('%d:%02d', (int)$minutes, (int)$seconds);
     }
 }
 
